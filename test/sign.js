157fec6b09cbfad5252c8a6d18767bb5ee7da784
Carsten Milling <cmil@hashtable.de>
2019-01-21 19:47:14 +0100

    add script creating webhook signatures

---
+++ test/sign.js
@@ -0,0 +1,19 @@
+#!/bin/env node
+
+// node sign.js secret datafile
+
+const fs = require('fs');
+const crypto = require('crypto');
+
+const secret = process.argv[2]
+const file = process.argv[3]
+console.log(file);
+
+const payload = fs.readFileSync(file, 'utf8');
+
+
+const hash = crypto.createHmac('sha1', secret)
+                   .update(payload)
+                   .digest('hex');
+
+console.log(`sha1=${hash}`);
