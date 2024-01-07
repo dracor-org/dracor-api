3bfb98557fc351cb67c7947d4689f196306d27a0
Carsten Milling <cmil@hashtable.de>
2024-01-07 18:45:51 +0100

    Use eXist URL for local schemathesis test

--- test/schemathesis.sh
+++ test/schemathesis.sh
@@ -1,6 +1,6 @@
 # This should reproduce the test in the GitHub workflow.
 
-schemathesis run http://localhost:8088/api/v1/openapi.yaml \
+schemathesis run http://localhost:8080/exist/restxq/v1/openapi.yaml \
   --checks not_a_server_error \
   --report \
   --hypothesis-max-examples 50 \
