be316c9a740f14c432c2357f4d5f6dced44b9500
Carsten Milling <cmil@hashtable.de>
2024-01-29 00:48:50 +0100

    Change test instance port to 8081

--- test/schemathesis.sh
+++ test/schemathesis.sh
@@ -1,6 +1,6 @@
 # This should reproduce the test in the GitHub workflow.
 
-schemathesis run http://localhost:8080/exist/restxq/v1/openapi.yaml \
+schemathesis run http://localhost:8081/exist/restxq/v1/openapi.yaml \
   --checks not_a_server_error,content_type_conformance,response_headers_conformance,response_schema_conformance \
   --report \
   --hypothesis-max-examples 50 \
