11cf273feaf6e4f3e180de18275e441b0ae079bb
Carsten Milling <cmil@hashtable.de>
2024-01-29 01:07:16 +0100

    Excluding status_code_conformance check for now
    It still yields several Bad Request errors.

--- test/schemathesis.sh
+++ test/schemathesis.sh
@@ -1,7 +1,7 @@
 # This should reproduce the test in the GitHub workflow.
 
 schemathesis run http://localhost:8081/exist/restxq/v1/openapi.yaml \
-  --checks not_a_server_error,content_type_conformance,response_headers_conformance,response_schema_conformance \
+  --exclude-checks status_code_conformance \
   --report \
   --hypothesis-max-examples 50 \
   --auth admin: \
