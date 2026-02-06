dracor-org/dracor-api

commit d2b65876042cb194307a0dc042c589bc867a290b

Make sure dutil:get-titles works with and without type="main"

modules/util.xqm
@@ -883,7 +883,7 @@ declare function dutil:get-authors($tei as node()) as map()* {
 declare function dutil:get-titles(
   $tei as element(tei:TEI)
 ) as map() {
-  let $title := $tei//tei:fileDesc/tei:titleStmt/tei:title[1]/normalize-space()
+  let $title := $tei//tei:fileDesc/tei:titleStmt/tei:title[not(@type) or @type = 'main'][1]/normalize-space()
   let $subtitle :=
     $tei//tei:titleStmt/tei:title[@type='sub'][1]/normalize-space()
   return map:merge((
@@ -907,7 +907,7 @@ declare function dutil:get-titles(
   else
   let $title :=
     $tei//tei:fileDesc/tei:titleStmt
-      /tei:title[@xml:lang = $lang and not(@type = 'sub')][1]
+      /tei:title[@xml:lang = $lang and (not(@type) or  or @type = 'main')][1]
       /normalize-space()
   let $subtitle :=
     $tei//tei:titleStmt/tei:title[@type = 'sub' and @xml:lang = $lang][1]

commit ca3c30d5ae55beccb7373efa78687e6dad81e18a

Fix dutil:get-titles to find main title with unsupported type
The change in d2b6587 broke the function in cases where a single
title has an attribute other than main.

see https://github.com/dracor-org/engdracor/issues/79

modules/util.xqm
@@ -883,7 +883,8 @@ declare function dutil:get-authors($tei as node()) as map()* {
 declare function dutil:get-titles(
   $tei as element(tei:TEI)
 ) as map() {
-  let $title := $tei//tei:fileDesc/tei:titleStmt/tei:title[not(@type) or @type = 'main'][1]/normalize-space()
+  let $title := $tei//tei:fileDesc/tei:titleStmt
+    /tei:title[not(@type = 'sub')][1]/normalize-space()
   let $subtitle :=
     $tei//tei:titleStmt/tei:title[@type='sub'][1]/normalize-space()
   return map:merge((