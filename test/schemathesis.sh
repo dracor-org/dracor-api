d9d5c36a1349f77138f39133c7f24a7682f73917
Carsten Milling <cmil@hashtable.de>
2026-07-12 19:24:04 +0200

    Exclude /wikidata/author/{id} from schemathesis tests
    Fuzzying this endpoint with random IDs unnecessarily floods the Wikidata
    SPARQL service.

--- test/schemathesis.sh
+++ test/schemathesis.sh
@@ -3,6 +3,7 @@
 schemathesis run http://localhost:8081/exist/restxq/v1/openapi.yaml \
   --include-method GET \
   --exclude-tag DTS \
+  --exclude-operation-id wikidata-author-info \
   --mode positive \
   --max-examples 50 \
   --request-timeout 30 \
